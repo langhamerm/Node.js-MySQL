36b5b44b59439cae26bf16a498d29fb462813a84
Mark Langhamer <langhamerm@gmail.com>
2019-01-12 12:27:17 -0600

    Final

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Node.js-MySQL
+![alt text](images/Add+Inventory.png?raw=true "Add Inventory");
+![alt text](images/Add+New+Item.png?raw=true "Add New Item");
+![alt text](images/FailedPurchase.png?raw=true "Failed Purchase");
+![alt text](images/LowInventory.png?raw=true "Low Inventory Check");
+![alt text](images/ManagerViewProducts.png?raw=true "Manager View All Products");
+![alt text](images/PurchaseProduct.png?raw=true "Purchase Products");
+![alt text](images/SelectProduct.png?raw=true "Select Product for Purchase");
+![alt text](images/ShowProducts.png?raw=true "Show All Product for Customer Purchase");
---
+++ bamazonManager.js
@@ -0,0 +1,188 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Pharmacy420!",
+    database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    managerOptions();
+});
+
+
+function managerOptions() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View Products for Sale",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        })
+        .then(function (answer) {
+            console.log(answer);
+            if (answer.action === "View Products for Sale") {
+                viewProducts();
+            }
+            else if (answer.action === "View Low Inventory") {
+                viewLow();
+            }
+            else if (answer.action === "Add to Inventory") {
+                addInventory();
+            }
+            else if (answer.action === "Add New Product") {
+                addProduct();
+            }
+        });
+}
+function viewLow() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].stock_quantity < 5) {
+                console.log(results[i].product_name)
+            }
+        }
+    })
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "Would you like to return to the menu?",
+        choices: [
+            "Yes",
+            "No"
+        ]
+    })
+    .then(function (answer) {
+        if (answer.action === "Yes") {
+            managerOptions();
+        }
+        else {
+            console.log("Goodbye for now!");
+            connection.end();
+        }
+    });
+}
+function viewProducts() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        console.log(results);
+    })
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "Would you like to return to the menu?",
+        choices: [
+            "Yes",
+            "No"
+        ]
+    })
+    .then(function (answer) {
+        if (answer.action === "Yes") {
+            managerOptions();
+        }
+        else {
+            console.log("Goodbye for now!");
+            connection.end();
+        }
+    });
+}
+function addInventory() {
+     // prompt for info about the item being put up for auction
+  inquirer
+  .prompt([
+    {
+      name: "product",
+      type: "input",
+      message: "What is the product you would like to add to?"
+    },
+    {
+      name: "stock",
+      type: "input",
+      message: "How many would you like in stock?"
+    }
+  ])
+  .then(function(answer) {
+    // when finished prompting, insert a new item into the db with that info
+    connection.query(
+      "UPDATE products SET ? WHERE ?",
+     [ {
+        stock_quantity: parseInt(answer.stock)
+
+      },
+      {
+        product_name: answer.product
+      }
+    ],
+      function(err) {
+        if (err) throw err;
+        console.log("Your inventory has been updated!");
+        managerOptions();
+      }
+    );
+  });
+}
+function addProduct() {
+    // prompt for info about the item being put up for auction
+    inquirer
+      .prompt([
+        {
+          name: "product",
+          type: "input",
+          message: "What is the product you would like to post?"
+        },
+        {
+          name: "department",
+          type: "input",
+          message: "What department would you like to place your product in?"
+        },
+        {
+          name: "price",
+          type: "input",
+          message: "Whats the price?",
+        },
+        {
+            name: "stock",
+            type: "input",
+            message: "How many units to add to stock?",
+          }
+      ])
+      .then(function(answer) {
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(
+          "INSERT INTO products SET ?",
+          {
+            product_name: answer.product,
+            department_name: answer.department,
+            price: answer.price,
+            stock_quantity: answer.stock
+          },
+          function(err) {
+            if (err) throw err;
+            console.log("Your product was added successfully!");
+            // re-prompt the user for if they want to bid or post
+            managerOptions();
+          }
+        );
+      });
+  }
